29517033fb81f15da45b9774a9d9a9721e7db0b5
Tianyi Zhao <tz1@andrew.cmu.edu>
2019-05-16 23:55:49 -0400

    add args

--- testBinary.bat
+++ testBinary.bat
@@ -6,6 +6,8 @@ SET MODELS_FOLDER=models
 SET GET_MODELS=.\getModels.bat
 SET DEMO=.\bin\OPenPoseDemo.exe
 SET ARGS=--video .\examples\media\video.avi
+SET ARGS_LOWRES=%ARGS% --net_resolution -1x80
+SET ARGS_CPU=ARGS% --render_pose 1
 
 echo ----- Entering binary root folder %BINARY_ROOT% -----
 IF EXIST %BINARY_ROOT% (
@@ -24,6 +26,11 @@ call %GET_MODELS%
 cd ..
 echo:
 
-echo ----- Running OpenPose Demo with video -----
+echo ----- Running OpenPose Demo in video mode -----
+:: With default settings
 %DEMO% %ARGS%
+:: With low resolution
+::%DEMO% %ARGS_LOWRES%
+:: With CPU mode
+::%DEMO% %ARGS_CPU%
 echo:
